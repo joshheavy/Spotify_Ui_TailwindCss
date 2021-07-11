05c4c487993d86e985bb9256d6a6922d2e0fe297
Joshheavy <waruriijoshua21@gmail.com>
2021-07-12 01:20:54 +0300

    initial ui

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      borderRadius: ["hover"],
+      backgroundClip: ["hover"],
+      display: ['hover']
+    },
+  },
+  plugins: [],
+};
